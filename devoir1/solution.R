805d8d718b023fe608021250a3bf8add5eb06999
Patrick Fournier <p_fournier@hushmail.com>
2021-09-14 18:07:08 -0400

    Met en ligne le devoir 1.

---
+++ devoir1/solution.R
@@ -0,0 +1,31 @@
+########################
+##     Exercice 1     ##
+########################
+
+gamma_mult <- function(x, p) {
+    stop("À compléter")
+}
+
+########################
+##     Exercice 2     ##
+########################
+
+det_update_chol<- function(x, d, R) {
+    stop("À compléter")
+}
+
+########################
+##     Exercice 3     ##
+########################
+
+wishart_pert <- function(n, M, m, Σ) {
+
+    stop("À compléter")
+
+    replicate(n, {
+
+        stop("À compléter")
+
+        list(M = stop("À compléter"), fM = stop("À compléter"))
+    })
+}
